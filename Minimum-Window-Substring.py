34bdce9a2d4c85e098b8d725ba74dd2f2c854329
Patrick Anyanwu <patrickchukwumaanyanwu@gmail.com>
2025-02-04 22:52:01 +0000

    Update Minimum-Window-Substring.py

--- Minimum-Window-Substring.py
+++ Minimum-Window-Substring.py
@@ -6,3 +6,34 @@ Once we have the correct frequency’s for all letters in the other string (our 
 After this  we increment l and reduce the frequency of the letter at l until we have a false substring then the loop continues. 
 We then return the string from the lower index to the higher index or we return an empty string. 
 O(n) time O(n) space."""
+
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        if t == "":
+            return ""
+
+        countT, window = {}, {}
+        for c in t:
+            countT[c] = 1 + countT.get(c, 0)
+
+        have, need = 0, len(countT)
+        res, resLen = [-1, -1], float("infinity")
+        l = 0
+        for r in range(len(s)):
+            c = s[r]
+            window[c] = 1 + window.get(c, 0)
+
+            if c in countT and window[c] == countT[c]:
+                have += 1
+
+            while have == need:
+                if (r - l + 1) < resLen:
+                    res = [l, r]
+                    resLen = r - l + 1
+                    
+                window[s[l]] -= 1
+                if s[l] in countT and window[s[l]] < countT[s[l]]:
+                    have -= 1
+                l += 1
+        l, r = res
+        return s[l : r + 1] if resLen != float("infinity") else ""
