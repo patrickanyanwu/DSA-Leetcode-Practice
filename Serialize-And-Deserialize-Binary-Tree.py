bc0ceaa3a847528942b026a7a552bc7e703b6dc0
Patrick Anyanwu <patrickchukwumaanyanwu@gmail.com>
2025-02-16 15:44:43 +0000

    Create Serialize-And-Deserialize-Binary-Tree.py

---
+++ Serialize-And-Deserialize-Binary-Tree.py
@@ -0,0 +1,34 @@
+"""To serialize we Run DFS preorder and when we hit a leaf node we add E to our return list that is joined into a string to indicate it is a leaf when being deserialized.
+Deserialize splits it back into a list to be iterated through recursively and the tree is rebuilt.
+O(n) time O(n) space."""
+
+class Codec:
+    def serialize(self, root: Optional[TreeNode]) -> str:
+        res = []
+
+        def dfs(node):
+            if not node:
+                res.append("N")
+                return
+            res.append(str(node.val))
+            dfs(node.left)
+            dfs(node.right)
+
+        dfs(root)
+        return ",".join(res)
+        
+    def deserialize(self, data: str) -> Optional[TreeNode]:
+        vals = data.split(",")
+        self.i = 0
+
+        def dfs():
+            if vals[self.i] == "N":
+                self.i += 1
+                return None
+            node = TreeNode(int(vals[self.i]))
+            self.i += 1
+            node.left = dfs()
+            node.right = dfs()
+            return node
+
+        return dfs()
