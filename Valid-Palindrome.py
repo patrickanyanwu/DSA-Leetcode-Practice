1e3b99e163277e28ea1668786bb3a1a822daa32e
Patrick Anyanwu <patrickchukwumaanyanwu@gmail.com>
2025-03-06 17:50:08 +0000

    Update Valid-Palindrome.py

--- Valid-Palindrome.py
+++ Valid-Palindrome.py
@@ -1,14 +1,33 @@
-"""For every character in the input string we make a new string with each character that is a letter or number. We then reverse that string and check if its equal to the normal string.
-O(n) time O(n) space."""
+"""For every character in the input string we set left and right pointers, if either of them are a space or not alphanumereic we move the appropriate pointer.
+If they are both alphanumeric we check if they are equal, if they are we move both pointers and if not we return False as it is not a palindrome.
+O(n) time O(1) space."""
 
 
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        prev_string = ""
-        for letter in s.lower():
-            if (letter >= "a" and letter <= "z") or (letter >= "0" and letter<= "9"):
-                prev_string += letter
-        rev_string = "".join(prev_string[::-1])
-        if prev_string == rev_string:
+        if s == " ":
             return True
-        return False
+        if len(s) == 1:
+            return True
+        l, r = 0, len(s) - 1
+        while l <= r:
+            if s[l] == " ":
+                l += 1
+                continue
+            if s[r] == " ":
+                r -= 1
+                continue
+            letter1 = s[l].lower()
+            letter2 = s[r].lower()
+            if letter1.isalnum() and letter2.isalnum():
+                if letter1 == letter2:
+                    l += 1
+                    r -= 1
+                else:
+                    return False
+            else:
+                if not letter1.isalnum():
+                    l += 1
+                if not letter2.isalnum():
+                    r -= 1
+        return Tr
