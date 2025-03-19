d4c34dc5e0395a5b6e8a0889503cbbbd4d43afbd
Patrick Anyanwu <patrickchukwumaanyanwu@gmail.com>
2025-03-19 16:46:14 +0000

    Create Function Composition.js

---
+++ Javascript 30/Function Composition.js
@@ -0,0 +1,18 @@
+/**
+ * @param {Function[]} functions
+ * @return {Function}
+ */
+var compose = function (functions) {
+  return function (x) {
+    if (functions.length === 0) return x;
+    let input = x;
+
+    for (let i = functions.length - 1; i >= 0; i--) {
+      const currFunc = functions[i];
+
+      input = currFunc(input);
+    }
+
+    return input;
+  };
+};
