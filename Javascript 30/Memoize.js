228fc8b3e81024ee27cb8435f0479571a78f62c1
Patrick Anyanwu <patrickchukwumaanyanwu@gmail.com>
2025-03-19 16:47:32 +0000

    Create Memoize.js

---
+++ Javascript 30/Memoize.js
@@ -0,0 +1,14 @@
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+function memoize(fn) {
+    const cache = {};
+    return function(...args) {
+        const key = JSON.stringify(args);
+        if (!(key in cache)) {
+            cache[key] = fn(...args);
+        }
+        return cache[key];
+    }
+}
