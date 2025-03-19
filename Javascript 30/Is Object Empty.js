81969b17e30907e6777f008d3384b1ba3e7a85cc
Patrick Anyanwu <patrickchukwumaanyanwu@gmail.com>
2025-03-19 16:52:32 +0000

    Create Is Object Empty.js

---
+++ Javascript 30/Is Object Empty.js
@@ -0,0 +1,11 @@
+/**
+ * @param {Object|Array} obj
+ * @return {boolean}
+ */
+var isEmpty = function(obj) {
+    if (Array.isArray(obj)) {
+        return obj.length === 0;
+    } else {
+        return Object.entries(obj).length === 0;
+    }
+};
