4748e62c85fde36a29b0d4208bd70303b61dc7c0
Patrick Anyanwu <patrickchukwumaanyanwu@gmail.com>
2025-03-23 13:37:55 +0000

    Create Clone-Graph.py

---
+++ Clone-Graph.py
@@ -0,0 +1,33 @@
+"""Use BFS through graph and we use a hashmap to map each old node to its copy, we make a new node for each node in the original graph.
+We then append all neighbours of the original graph node to our copy node.
+
+O(V + E) time, O(V) space"""
+
+
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+class Solution:
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        if not node:
+            return None
+
+        oldToNew = {}
+        oldToNew[node] = Node(node.val)
+        q = deque([node])
+
+        while q:
+            cur = q.popleft()
+            for nei in cur.neighbors:
+                if nei not in oldToNew:
+                    oldToNew[nei] = Node(nei.val)
+                    q.append(nei)
+                oldToNew[cur].neighbors.append(oldToNew[nei])
+
+        return oldToNew[node]
+        
