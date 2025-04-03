1f9120a3076ee2371fbece777196f706815d4496
Patrick Anyanwu <patrickchukwumaanyanwu@gmail.com>
2025-04-03 20:22:30 +0100

    Update Palindromic-Substrings.py

--- Palindromic-Substrings.py
+++ Palindromic-Substrings.py
@@ -1,9 +1,15 @@
-
+"""
+  For each index in our word we start with 2 pointers. The algorithm starts from the index and expands outwards to check for palindromes.
+  A countmis initialized at the beginging which we will increment accordingly.
+  for the case of odd (in length) palindromes we start the 2 pointers on the same character,
+  for the case of even (in length) palindromes we start r one after l.
+  We then return the count.
+  O(n ^ 2) time O(1) space.
+"""
 
 class Solution:
     def countSubstrings(self, s: str) -> int:
         count = 0
-
         for i in range(len(s)):
             l, r = i, i
             while l >= 0 and r < len(s) and s[l] == s[r]:
