b38f880957d77fde8b7455fc17bc010c1c477c1e
Patrick Anyanwu <patrickchukwumaanyanwu@gmail.com>
2025-04-11 18:15:47 +0100

    Update Reverse-Bits.py

--- Reverse-Bits.py
+++ Reverse-Bits.py
@@ -1,4 +1,6 @@
-"""Initialize a result string, now i loop bit by bit checking if the bit is a 1 or 0 and add it to our res from right to left,
+"""
+  Initialize a result string, now i loop bit by bit from right to left appending each bit to res,
+  I know if its a 1 or 0 by using the mod 2 to get the last digit and anding by 1.
   After the loop i keep adding 0's untill the length of res is 32 (we have all bits).
   I then convert the string to a base 2 integer and return.
   Works in constant space as the res string will never be greater than 32.
