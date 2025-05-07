45dea48b0c0bdc7aff13e78f379a43e0463227e8
Patrick Anyanwu <patrickchukwumaanyanwu@gmail.com>
2025-05-07 15:17:09 +0100

    Update Equal-Row-and-Column-Pairs.py

--- Equal-Row-and-Column-Pairs.py
+++ Equal-Row-and-Column-Pairs.py
@@ -1,5 +1,5 @@
 """
-  Firstly we store a count of the frwuency of every row in a hashmap,
+  Firstly we store a count of the frequency of every row in a hashmap,
   next we loop through each column and check if that column is in the hashmap (we have a valid pair),
   if it is we increment our count of pairs by the frequency of the row in the hashmap (the number of pairs we can make with that column).
   O(n ^ 2) time O(n ^ 2) space.
