43a56d2cae9a6c1555a8b2d78c5c986244cfa63c
Patrick Anyanwu <patrickchukwumaanyanwu@gmail.com>
2025-09-08 18:47:42 +0100

    Create Reverse-Linked-List-II.py

---
+++ Reverse-Linked-List-II.py
@@ -0,0 +1,35 @@
+"""
+  Create a dummy node for the edge case of left being 1,
+  we then find the node before the beginning of the nodes we need to reverse,
+  we thenn reverse the linked list in place right - left times and return the dummy.next.
+  O(n) time O(1) space.
+"""
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
+        if not head or left == right:
+            return head
+
+        dummy = ListNode(0)
+        dummy.next = head
+        prev = dummy
+
+        for _ in range(left - 1):
+            prev = prev.next
+
+        curr = prev.next
+        nxt = None
+
+        for _ in range(right - left):
+            nxt = curr.next
+            curr.next = nxt.next
+            nxt.next = prev.next
+            prev.next = nxt
+
+        return dummy.next
