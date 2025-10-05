da0a8e953e81e852d884bc19169f5c9b40b05df7
Patrick Anyanwu <patrickchukwumaanyanwu@gmail.com>
2025-10-05 16:46:05 +0100

    Create LRU-Cache.py

---
+++ Questions & Solutions/LRU-Cache.py
@@ -0,0 +1,52 @@
+"""
+  We use 2 data structures, a doubly linked list and a hashmap which maps key to what node in the doubly linked list holds the value.
+  Every time we 'put' if the key is already in the cache we firstly remove it from the chache, then we create a new node and insert the node to the
+  end of the list. If the list is over capacity we remove the lists tail which is the Least Recently Used item.
+  On every get we just remove the node with that key and insert the node at the end of the list every time by updating pointers/
+"""
+
+class Node:
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+        self.prev = None
+        self.next = None
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.cache = {}
+        self.capacity = capacity
+        self.head = Node(0, 0)
+        self.tail = Node(0, 0)
+        self.head.next = self.tail
+        self.tail.prev = self.head
+
+    def _remove(self, node):
+        prev, nxt = node.prev, node.next
+        prev.next, nxt.prev = nxt, prev
+
+    def _insert(self, node):
+        node.next = self.head.next
+        node.prev = self.head
+        self.head.next.prev = node
+        self.head.next = node
+
+    def get(self, key: int) -> int:
+        if key not in self.cache:
+            return -1
+        node = self.cache[key]
+        self._remove(node)
+        self._insert(node)
+        return node.value
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            self._remove(self.cache[key])
+        node = Node(key, value)
+        self._insert(node)
+        self.cache[key] = node
+
+        if len(self.cache) > self.capacity:
+            lru = self.tail.prev
+            self._remove(lru)
+            del self.cache[lru.key]
