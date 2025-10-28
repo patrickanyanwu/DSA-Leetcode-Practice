c477901b96085a2ac233b18a2b453237be288aa9
Patrick Anyanwu <patrickchukwumaanyanwu@gmail.com>
2025-10-28 12:28:05 +0000

    Solved: Next-Permutation

---
+++ Questions & Solutions/Next-Permutation.py
@@ -0,0 +1,31 @@
+"""
+    I found the first decreasing element from
+    the right, swapped it with the next larger
+    element to its right, then reversed the
+    suffix to get the next lexicographically
+    larger permutation.
+    O(n) time O(1) space
+"""
+
+class Solution:
+    def nextPermutation(self, nums: list[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+        i = n - 2
+        
+        while i >= 0 and nums[i] >= nums[i + 1]:
+            i -= 1
+        
+        if i >= 0:
+            j = n - 1
+            while nums[j] <= nums[i]:
+                j -= 1
+            nums[i], nums[j] = nums[j], nums[i]
+        
+        left, right = i + 1, n - 1
+        while left < right:
+            nums[left], nums[right] = nums[right], nums[left]
+            left += 1
+            right -= 1
