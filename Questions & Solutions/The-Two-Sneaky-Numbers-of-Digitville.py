2a349249c6b2874111d7b973ce8f9d8aeeded8cc
Patrick Anyanwu <patrickchukwumaanyanwu@gmail.com>
2025-11-01 22:38:34 +0000

    Solved: The-Two-Sneaky-Numbers-of-Digitville

--- Questions & Solutions/The-Two-Sneaky-Numbers-of-Digitville.py
+++ Questions & Solutions/The-Two-Sneaky-Numbers-of-Digitville.py
@@ -1,19 +1,33 @@
 """
-    I used a set to track numbers I've seen as
-    I traversed the array. When I encountered a
-    number already in the set, I added it to
-    the result and returned early once I found
-    two duplicates.
-    O(n) time O(n) space
+    I used XOR to find the two duplicates. I
+    XORed all numbers and their expected values
+    to isolate the duplicates, then used a
+    differentiating bit to separate them into
+    two groups and XOR each group separately.
+    O(n) time O(1) space
 """
 
 class Solution:
     def getSneakyNumbers(self, nums: List[int]) -> List[int]:
-        seen = set()
-        res = []
+        x = 0
         for num in nums:
-            if num in seen:
-                res.append(num)
-                if len(res) == 2:
-                    return res
-            seen.add(num)
+            x ^= num
+        for i in range(len(nums) - 2):
+            x ^= i
+
+        diff_bit = x & -x
+        xor1 = xor2 = 0
+
+        for num in nums:
+            if num & diff_bit:
+                xor1 ^= num
+            else:
+                xor2 ^= num
+
+        for i in range(len(nums) - 2):
+            if i & diff_bit:
+                xor1 ^= i
+            else:
+                xor2 ^= i
+                
+        return [xor1, xor2]
