88e619c68002e86dacdc7e7e39c17f4c87e70362
Patrick Anyanwu <patrickchukwumaanyanwu@gmail.com>
2026-02-21 12:36:38 +0000

    Solved: Permutation-in-String.py

---
+++ Questions & Solutions/Permutation-in-String.py
@@ -0,0 +1,44 @@
+"""
+  Use a sliding window of size len(s1) on s2 to check for permutations.
+  Track character frequencies with two arrays (s1Count and s2Count).
+  Keep a 'matches' counter to track how many of the 26 letters have matching frequencies.
+  If matches == 26, all character frequencies match, meaning we found a permutation of s1 in the current window.
+  Slide the window across s2, incrementally updating matches by adding the new character and removing the old one.
+  O(26*n) = O(n) time, O(1) space.
+"""
+
+
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s1) > len(s2):
+            return False
+
+        s1Count, s2Count = [0] * 26, [0] * 26
+        for i in range(len(s1)):
+            s1Count[ord(s1[i]) - ord('a')] += 1
+            s2Count[ord(s2[i]) - ord('a')] += 1
+
+        matches = 0
+        for i in range(26):
+            matches += (1 if s1Count[i] == s2Count[i] else 0)
+
+        l = 0
+        for r in range(len(s1), len(s2)):
+            if matches == 26:
+                return True
+
+            index = ord(s2[r]) - ord('a')
+            s2Count[index] += 1
+            if s1Count[index] == s2Count[index]:
+                matches += 1
+            elif s1Count[index] + 1 == s2Count[index]:
+                matches -= 1
+
+            index = ord(s2[l]) - ord('a')
+            s2Count[index] -= 1
+            if s1Count[index] == s2Count[index]:
+                matches += 1
+            elif s1Count[index] - 1 == s2Count[index]:
+                matches -= 1
+            l += 1
+        return matches == 26
